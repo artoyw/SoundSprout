f30c0978cae708b745cd67b4013756c6606a5824
artoyw <artoyw@gmail.com>
2025-03-29 20:53:31 -0500

    stuff

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+import Liveplay from "./components/Liveplay";
+import Progress from "./components/Progress";
+import Tuner from "./components/Tuner";
+
+function App() {
+  return (
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/liveplay" element={< Liveplay />} />
+          <Route path="/progress" element={< Progress />} />
+          <Route path="/tuner" element={< Tuner />} />
+        </Routes>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Liveplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Liveplay() {
+  return (
+    <div >
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default Liveplay;
