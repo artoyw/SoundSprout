94cd5bd198c0fa7bb12822756174a2af5ad0f73a
artoyw <artoyw@gmail.com>
2025-03-29 21:09:58 -0500

    setup stuffs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Liveplay from "./components/Liveplay";
 import Progress from "./components/Progress";
 import Tuner from "./components/Tuner";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
         <Routes>
           <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={< Homepage />} />
           <Route path="/liveplay" element={< Liveplay />} />
           <Route path="/progress" element={< Progress />} />
           <Route path="/tuner" element={< Tuner />} />
